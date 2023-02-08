a156728930fa49759ececfa6d08f309abfd1b7e4
tan.tn <tan.tn@neo-lab.vn>
2023-02-08 14:02:10 +0700

    build docker php8 in wp

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
 
-try {
-    echo 'Current PHP version: ' . phpversion();
-    echo '<br />';
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    $host = 'db';
-    $dbname = 'database';
-    $user = 'user';
-    $pass = 'pass';
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-    $conn = new PDO($dsn, $user, $pass);
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    echo 'Database connected successfully';
-    echo '<br />';
-} catch (\Throwable $t) {
-    echo 'Error: ' . $t->getMessage();
-    echo '<br />';
-}
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
